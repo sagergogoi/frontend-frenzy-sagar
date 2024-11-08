a32aa4eb2faf9fb25f3f2a50fb894ba91dcfe960
Sagar . <fanindragogoi@gmail.com>
2024-11-08 18:23:23 +0530

    commit initial

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html','./rest.html'],
+  theme: {
+    extend: {
+      fontFamily:{
+        'display':['poppins','sans-serif'],
+        'body':['Inter','sans-serif']
+      }
+      
+    },
+  },
+  plugins: [],
+}
+
